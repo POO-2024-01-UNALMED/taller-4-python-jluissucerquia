334c8d1725c71032d6131591ccd221f843f3018b
jluissucerquia <jsucerquia@unal.edu.co>
2024-03-04 13:25:28 -0500

    :+1:

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -4,5 +4,10 @@ class Asignatura:
         self._nombre = nombre
         self._salon = salon
 
+
     def __str__(self):
-        return self._nombre + "" + self._salon
+        if self._salon == "remoto":
+            return self._nombre + "" + self._salon
+        else:
+            return self._nombre + "" + self._salon 
+        
